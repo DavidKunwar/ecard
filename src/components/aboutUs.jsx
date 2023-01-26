3766f4de88889306e16a9a46ed10eb51ae88d6c4
David Kunwar <dkunwar7111@gmail.com>
2023-01-26 22:28:49 +0530

    just testing if push works

--- src/components/aboutUs.jsx
+++ src/components/aboutUs.jsx
@@ -6,7 +6,7 @@ function AboutUs(){
     return (
         <div className='section' id='about'>
             <SectionTitle title='ABOUT US' />
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias fugiat consequuntur officia quis corrupti error deleniti maiores aliquid eum deserunt. Incidunt quibusdam voluptatibus distinctio quae blanditiis, ut laboriosam placeat ea sint unde nisi commodi praesentium atque laborum delectus porro sapiente voluptates beatae autem illum ab assumenda aperiam modi. Sint, optio!</p>
+            <p>asdf Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias fugiat consequuntur officia quis corrupti error deleniti maiores aliquid eum deserunt. Incidunt quibusdam voluptatibus distinctio quae blanditiis, ut laboriosam placeat ea sint unde nisi commodi praesentium atque laborum delectus porro sapiente voluptates beatae autem illum ab assumenda aperiam modi. Sint, optio!</p>
         </div>
     )
 }
