d2545fdf2265e8fce3b261cf34a4357284f42421
David Kunwar <dkunwar7111@gmail.com>
2023-03-13 15:50:49 +0530

    upi added

--- src/components/paymentInfo.jsx
+++ src/components/paymentInfo.jsx
@@ -1,29 +1,13 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import SectionTitle from './sectionTitle'
+import paytmCover from '../Images/paytm.jpeg'
 
 function PaymentInfo() {
-
-    const loadScript = (src) => {
-        const form = document.createElement("form")
-        const script = document.createElement("script");
-        script.src = src;
-        script.setAttribute('data-payment_button_id', 'pl_LNPfJEzYT38wxO');
-        script.async = true
-        script.classList.add('payment-btn');
-
-        form.appendChild(script);
-        // document.body.appendChild(form);
-        document.getElementById('payment-info').append(form)
-    }
-
-    useEffect(() => {
-        loadScript("https://checkout.razorpay.com/v1/payment-button.js")
-    })
 
     return (
         <div className='section' id='payment-info'>
             <SectionTitle title='Payment Info' />
-            <div></div>
+            <img src={paytmCover} alt="" />
         </div>
     )
 }
